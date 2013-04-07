a12c9df55a20dd85b6d0185b9781485291b40084
Kyle Kelley <rgbkrk@gmail.com>
2013-04-07 00:33:07 -0400

    Quick profiling notes for IPython section

---
+++ ch03/prof_mod.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from numpy.random import randn
+
+def add_and_sum(x,y):
+    added = x + y
+    summed = added.sum(axis=1)
+    return summed
+
+def call_function():
+    x = randn(1000, 1000)
+    y = randn(1000, 1000)
+    return add_and_sum(x,y)
